e6511b33126f9c3b6fbcdf73650366cf5693c5fc
Andrew Fernandez <waltershowalter@users.noreply.github.com>
2026-08-13 14:06:31 -0600

    Update config.js with new invoke URL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://vwwbp7ooie.execute-api.us-west-2.amazonaws.com/devtestaugust2026' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
